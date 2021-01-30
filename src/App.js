12a05c6a87ea4e06c0118b4d8dd1e3c2f99508ad
Sedki-Sghairi <sedkisghairi3@gmail.com>
2021-01-30 22:49:01 +0400

    first setup

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { useState, useEffect } from 'react';
+import { FiChevronRight, FiChevronLeft } from 'react-icons/fi';
+import { FaQuoteRight } from 'react-icons/fa';
+import data from './data';
+function App() {
+	return (
+		<section className="section">
+			<div className="title">
+				<h2>
+					<span />reviews
+				</h2>
+			</div>
+			<div className="section-center" />
+		</section>
+	);
+}
+
+export default App;
